62395b0bb67715042392031140273c1325fe64c3
t0klsa00 <80824045+t0klsa00@users.noreply.github.com>
2021-11-18 14:30:41 +0200

    testin the database

--- src/merchants.js
+++ src/merchants.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 5432
 
 const merchant_model = require('./merchant_model')
 
 app.use(express.json())
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin', 'https://projectawa.herokuapp.com/');
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
   next();
