3cddb4666f93a734609413bcad7978fc85ffa2a8
t0klsa00 <80824045+t0klsa00@users.noreply.github.com>
2021-11-18 14:00:43 +0200

    trying database

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Mercapp from './merchantsapp';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Mercapp />, document.getElementById('root')
   </React.StrictMode>,
   document.getElementById('root')
 );
