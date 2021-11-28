916aa2f2750f23725ff650eae5d84d6892356506
t0klsa00 <80824045+t0klsa00@users.noreply.github.com>
2021-11-28 16:03:21 +0200

    updating views

--- src/RestaurantList.js
+++ src/RestaurantList.js
@@ -1,7 +1,57 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { Link, Outlet } from 'react-router-dom'
 
 export default function RestaurantList(props) {
+
+  // NEW MESSY CODE FOR RESTAURANT DB //
+
+  const [restauranties, setRestaurants] = useState(false);
+  useEffect(() => {
+    getRestaurant();
+  }, []);
+  function getRestaurant() {
+    fetch('http://localhost:3001')
+      .then(response => {
+        return response.text();
+      })
+      .then(data => {
+        setRestaurants(data);
+      });
+  }
+  function createRestaurant() {
+    let name = prompt('Enter restaurant name');
+    let address = prompt('Enter restaurant address');
+    fetch('http://localhost:3001/restaurant', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({name, address}),
+    })
+      .then(response => {
+        return response.text();
+      })
+      .then(data => {
+        alert(data);
+        getRestaurant();
+      });
+  }
+  function deleteRestaurant() {
+    let id = prompt('Enter restaurant id');
+    fetch(`http://localhost:3001/restaurant/${id}`, {
+      method: 'DELETE',
+    })
+      .then(response => {
+        return response.text();
+      })
+      .then(data => {
+        alert(data);
+        getRestaurant();
+      });
+  }
+
+  // NEW MESSY CODE FOR RESTAURANT DB ENDS HERE //
+  
   return (
     <div className="restaurantListView">
       <div className="restaurantList">
@@ -15,6 +65,19 @@ export default function RestaurantList(props) {
         INFO
         <Outlet />
       </div>
+
+      // NEW MESSY CODE FOR RESTAURANT DB //
+
+      <div>
+      {restauranties ? restauranties : 'There is no restaurant data available'}
+      <br />
+      <button onClick={createRestaurant}>Add restaurant</button>
+      <br />
+      <button onClick={deleteRestaurant}>Delete restaurant</button>
+    </div>
+
+        // NEW MESSY CODE FOR RESTAURANT DB ENDS HERE //
+
     </div>
   )
 }
