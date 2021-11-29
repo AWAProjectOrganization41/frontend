4a95a4c231f24ebc0fc7e5d50f4d6cff4257d393
Juho-beeb <juho_harvala@outlook.com>
2021-11-29 17:42:45 +0200

    Commit

--- src/LoginConsumer.js
+++ src/LoginConsumer.js
@@ -1,13 +1,47 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-
+import React, { useState } from 'react';
+import LoginForm from './components/LoginForm';
 export default function LoginConsumer() {
+  const adminUser = {
+    email:"admin@123.com",
+    password: "123"
+  }
+
+  const [user, setUser] = useState({name: "", email:""});
+  const [error, setError] = useState("");
+  const Login = details => {
+    console.log(details);
+
+    if(details.email === adminUser.email && details.password === adminUser.password){
+      console.log("Kirjauduttu sisään");
+      setUser({
+        name: details.name,
+        email: details.email
+      })}
+  
+      else {
+        console.log("Kirjautuminen epäonnistui");
+    }
+    
+  
+  }
+
+  const Logout = () => {
+    setUser({name: "", email:""});
+  }
+
+
   return (
-    <div>
-      Login as a consumer
-      <h4>Käyttäjätunnus</h4>
-      <h4>Salasana</h4>
-      <Link to="/restaurants"><div>Kirjaudu sisään</div></Link>
+    <div className="App">
+      {(user.email != "") ? (
+        <div className="welcome">
+          <h2>Tervetuloa <span>{user.name}</span> </h2>
+          <Link to="/restaurants"><div>Kirjaudu sisään</div></Link>
+          <button onClick={Logout}>Kirjaudu ulos</button>
+        </div> ) : (
+      <LoginForm Login={Login} error={error}/>
+     )}
     </div>
-  )
-}
+  );
+  }
+
+export default App;
