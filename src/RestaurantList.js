94e43a11eb2f67f1f845b971c3555c00f5e1ed72
t0klsa00 <80824045+t0klsa00@users.noreply.github.com>
2021-12-01 22:51:54 +0200

    estimated arrival map deployed

--- src/RestaurantList.js
+++ src/RestaurantList.js
@@ -6,11 +6,12 @@ export default function RestaurantList(props) {
 
   return (
     <div className={ styles.restaurantList }>
-      <div className={ styles.restaurantList }>
+      <img style={{width:'100%'}} src={'./images/arrival.png'} />
+      <div>
       { props.restaurants.map(restaurant =>
         <Link to={ "/restaurants/" + restaurant.restaurant_id}>
           <div className={ styles.product }>
-            <div><img src={`./images/${restaurant.imagepath}`} /></div>{restaurant.name}<div>{restaurant.address}</div><div>{restaurant.restaurant_id}</div></div>
+            <div><img className={ styles.image }src={`./images/${restaurant.imagepath}`} /></div><div className={ styles.header }>{restaurant.name}</div><div>{restaurant.address}</div><div>{restaurant.restaurant_id}</div></div>
         </Link>
       )}
       </div>
