c7e769dc8ed5915802122d6fa169a1ee0c1d7908
veerakatariin <t0pive00@students.oamk.fi>
2021-12-01 19:05:46 +0200

    add restaurant created

--- src/RestaurantUI.js
+++ src/RestaurantUI.js
@@ -3,11 +3,77 @@ import React, {useState, useEffect} from 'react'
 
 export default function RestaurantUI(){
 
+    const [restaurant, setRestaurant] = useState({name:"", address:"", hours:"", imagepath:"", type:"", pricelevel:""});
+
+    console.log(restaurant);
+
+    const submitHandler = e => {
+        e.preventDefault();
+
+    }
+
+    
     return(
 
        <div>
-           <p> moro </p>
+           <h2> Add a restaurant </h2>
+           <br/>
+           <section>
+               <label for="name"/> Enter a name <label/>
+               <input type="text" name="name" id="name" onChange= { e => setRestaurant({ name: e.target.value})} value={restaurant.name}></input>
+               <br/><br/>
+
+               <label for="address"/> Enter an address <label/>
+               <input type="text" name="address" id="address" onChange= { e => setRestaurant({ address: e.target.value})} value={restaurant.address}></input>
+               <br/><br/>
+
+               <label for="hours"/> Enter the operating hours <label/>
+               <input type="text" name="hours" id="hours" onChange= { e => setRestaurant({ hours: e.target.value})} value={restaurant.hours}></input>
+               <br/><br/>
+
+               <label for="imagepath"/> Enter a file path for your image <label/>
+               <input type="text" name="imagepath" id="imagepath" onChange= { e => setRestaurant({ imagepath: e.target.value})} value={restaurant.imagepath}></input>
+               <br/><br/>
+
+
+               <span> Enter the restaurant type </span>
+               <br/>
+               <input type="radio" name="type" id="buffet" value="buffet" onChange= { e => setRestaurant({ type: e.target.value})} />
+               <label for="buffet"> Buffet </label>
+
+               <input type="radio" name="type" id="fastfood" value="fastfood" onChange= { e => setRestaurant({ type: e.target.value})}/>
+               <label for="fastfood"> Fast food </label>
+
+               <input type="radio" name="type" id="fastcasual" value="fastcasual" onChange= { e => setRestaurant({ type: e.target.value})}/>
+               <label for="fastcasual"> Fast casual </label>
+
+               <input type="radio" name="type" id="casualdining" value="casualdining" onChange= { e => setRestaurant({ type: e.target.value})}/>
+               <label for="casualdining"> Casual dining </label>
+
+               <input type="radio" name="type" id="finedining" value="finedining" onChange= { e => setRestaurant({ type: e.target.value})}/>
+               <label for="finedining"> Fine dining </label>
+               <br/><br/>
+
+               <span> Enter the price level</span>
+               <br/>
+               <input type="radio" name="price" id="price1" value="€" onChange= { e => setRestaurant({ type: e.target.value})}/>
+               <label for="price1"> € </label>
+
+               <input type="radio" name="price" id="price2" value="€€" onChange= { e => setRestaurant({ type: e.target.value})}/>
+               <label for="price2"> €€ </label>
+
+               <input type="radio" name="price" id="price3" value="€€€" onChange= { e => setRestaurant({ type: e.target.value})}/>
+               <label for="price3"> €€€ </label>
+
+               <input type="radio" name="price" id="price4" value="€€€€" onChange= { e => setRestaurant({ type: e.target.value})}/>
+               <label for="price4"> €€€€ </label>
+
+               
+           </section>
+
+           
        </div> 
     );
 
 }
+
