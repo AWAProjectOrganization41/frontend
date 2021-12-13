54764aaaf89e5991a5928b5c22b9e890488f6ede
veerakatariin <t0pive00@students.oamk.fi>
2021-12-13 04:59:36 +0200

    restaurantdetailview cleaned

--- src/RestaurantDetailView.js
+++ src/RestaurantDetailView.js
@@ -22,32 +22,15 @@ export default function RestaurantDetailView(props, showContent) {
   //const restaurantid = localStorage.getItem('restaurantdetailkey'+)
 
   var user_key = localStorage.getItem('user_key')
-  console.log(JSON.parse(user_key))
   let key = JSON.parse(user_key)
 
-// funktio hakkee menut. Tähän pitää lisätä id minkä mukaan hakee:
-  
 
   useEffect(() => {
     getRestaurant();
-    //getMenu();
     getMenuById();
   }, []);
 
-  /*function getMenu() {
-    fetch('http://localhost:3001/restaurant_menu') // http://localhost:3001/restaurant_menu jos lokaalisti, /restaurant_menu jos heroku
-    .then(response => {
-      return response.text();
-    })
-    .then(data => {
-      console.log(JSON.stringify(data));
-      setMenu(JSON.parse(data))
-    });
-  }*/
-
-
   function getMenuById(){
-    console.log(result.restaurant_id + "joooo");
 const idarr = [{id: result.restaurant_id}]
     fetch('http://localhost:3001/restaurant_menu', { 
     method: 'POST',
@@ -60,7 +43,6 @@ const idarr = [{id: result.restaurant_id}]
       return response.text();
     })
     .then(data => {
-      console.log("tata:"+JSON.parse(data))
       setMenu(JSON.parse(data))
     });
   }
@@ -72,25 +54,19 @@ function getRestaurant() {
     return response.text();
   })
   .then(data => {
-    console.log("mooi" + data)
     setRestaurants(JSON.parse(data));
   });
 }
 
 // status joka määrittelee, näytetäänkö menu vai ostoskori. (myöhemmin shoppingcart omalle sivulle?)
-
     const [ViewStatus, setStatus] = useState('')
     
-  // Händlää tuotteen lisäämisen listalle cart_items:
 
+  // Händlää tuotteen lisäämisen listalle cart_items:
     const handleFoodClick = (menui) => {
-      //cart_items[i] = SortedCart.products = menui
       
-      console.log("HANDLEFOODCLICK")
       
       if(cart_items.includes(menui)){
-        console.log("handling "+JSON.stringify(menui))
-        console.log("Handling "+JSON.stringify(cart_items))
           setStatus('menu_view');
       }
   
@@ -102,19 +78,12 @@ function getRestaurant() {
     };}
 
     const handleDeleteFromCart = (item) => {    // Status: näytetään ostoskori
-      console.log("HANDLEDELETE")
-      console.log("poistetaan: "+JSON.stringify(item))
       let a = 0
-      console.log("1"+cart_items)
-      console.log("1"+JSON.stringify(cart_items))
       cart_items.forEach(function(message){
-        //console.log(message)
+   
         if (message === item){
-          console.log("onsama"+JSON.stringify(message) + JSON.stringify(item))
-          console.log("onsamaitem"+cart_items[a])
-          console.log("byt poistuu index: "+JSON.stringify(cart_items[a]))
+
               cart_items.splice(cart_items.indexOf(a),1)
-              console.log(cart_items)
               i = 0            
               setStatus('shoppingcart');             
             }
@@ -127,7 +96,6 @@ function getRestaurant() {
   const result = useParams();
   const restaurant = all_restaurants.find(restaurant => restaurant.restaurant_id === parseInt(result.restaurant_id));
   if(restaurant == null) {
-    console.log("terve" + result.restaurant_id);
     return <div>No matching restaurant</div>
   }
 
@@ -164,11 +132,11 @@ function getRestaurant() {
         </div>
       )}
       else{
-      console.log("CARTVIEW")
+
 
       var summa = 0
       cart_items.map(money => summa = summa+parseFloat(money.price))
-      console.log("tässä näkyy" + JSON.stringify(cart_items) + summa)
+
     
       localStorage.setItem('shoppincart', JSON.stringify(cart_items)+'...'+restaurant.name+'...'+restaurant.owner_id);
     return(<div className={styles.shoppingcartContainer}>
